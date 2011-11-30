f13aa731be6a5cbeaeb6580bc29ad1f2435cd6df
senlin <senlin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-30 12:54:00 +0000

    Tagging version 1.0 of Dashboard Tweaks.
    git-svn-id: https://plugins.svn.wordpress.org/dashboard-tweaks/tags/1.0@469453 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dashboard-tweaks.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Plugin Name: Dashboard Tweaks
+Plugin URI: http://wpti.ps/category/plugins/
+Description: A Collection of Tweaks (Functions & CSS) for WordPress version 3.3 and higher. Based on work by Siobhan (http://senl.in/u6kifV) 
+Author: Piet Bos
+Version: 1.0
+Author URI: http://wpti.ps
+License: GPLv2
+*/
+
+/*  Copyright 2011  Piet Bos  (email: senlinonline@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// For debugging purposes
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1); 
+//define('WP-DEBUG', true);
+
+register_activation_hook( __FILE__, 'sl_dashboard_tweaks_install' );
+
+// If < WP 3.3-beta4 die nicely (source: http://www.presscoders.com/2011/11/deactivate-wordpress-plugin-automatically/)
+function sl_requires_wordpress_version() {
+	global $wp_version;
+	$plugin = plugin_basename( __FILE__ );
+	$plugin_data = get_plugin_data( __FILE__, false );
+ 
+	if ( version_compare($wp_version, "3.3-beta4", "<" ) ) {
+		if( is_plugin_active($plugin) ) {
+			deactivate_plugins( $plugin );
+			wp_die( "'".$plugin_data['Name']."' requires WordPress 3.3 or higher! Deactivating Plugin.<br /><br />Back to <a href='".admin_url()."'>WordPress admin</a>." );
+		}
+	}
+}
+add_action( 'admin_init', 'sl_requires_wordpress_version' );
+
+add_action( 'init', 'sl_dashboard_tweaks_init' );
+
+function sl_dashboard_tweaks_init() { load_plugin_textdomain( 'sl_dashtweaks' ); }
+
+function sl_dashboard_tweaks() {
+	echo '<link rel="stylesheet" type="text/css" href="' .plugins_url('css/wp-admin.css', __FILE__). '">';
+}
+add_action('admin_head', 'sl_dashboard_tweaks');
+
+// Remove WordPress sub-menu from the admin bar, add custom admin logo instead and remove "Visit Site" sub-menu under site-name.
+function sl_dashboard_tweaks_render() {
+	global $wp_admin_bar;
+	$wp_admin_bar->add_menu( array(
+		'id'    => 'wp-logo',
+		'title' => '<span class="ab-wp-logo"></span>',
+		'href'  => is_admin() ? home_url( '/' ) : admin_url(),
+	) );
+	$wp_admin_bar->remove_menu('about');
+	$wp_admin_bar->remove_menu('wporg');
+	$wp_admin_bar->remove_menu('documentation');
+	$wp_admin_bar->remove_menu('support-forums');
+	$wp_admin_bar->remove_menu('feedback');
+	$wp_admin_bar->remove_menu('view-site');
+}
+add_action( 'wp_before_admin_bar_render', 'sl_dashboard_tweaks_render' );
+
+// Change "Howdy" into "Welcome back" (source: http://wpmu.org/daily-tip-how-to-change-the-wordpress-howdy-message-to-a-custom-welcome/)
+function change_howdy($translated, $text, $domain) {
+    if (!is_admin() || 'default' != $domain)
+        return $translated;
+    if (false !== strpos($translated, 'Howdy'))
+        return str_replace('Howdy', 'Welcome back', $translated);
+    return $translated;
+}
+add_filter('gettext', 'change_howdy', 10, 3);
+
+// Rebrand the title at the top of all pages in wp-admin (source: http://www.artofblog.com/white-labeling-wordpress/#comment-32486)
+function sl_new_wp_title() {
+$new_title= __('Dashboard of ', 'sl_dashtweaks') . get_option('blogname');
+return $new_title;
+}
+add_filter('admin_title', 'sl_new_wp_title');
+
+// Customize admin footer text
+function sl_admin_footer() {
+	echo __('The Dashboard Tweaks plugin has been developed by Piet Bos of <a href="http://wpti.ps">WP TIPS</a>. For any issues, please open a thread in the Wordpress plugin forum.', 'sl_dashtweaks');
+} 
+add_filter('admin_footer_text', 'sl_admin_footer');
+
+// Add a favicon for your admin
+function sl_admin_favicon() {
+	echo '<link rel="Shortcut Icon" type="image/x-icon" href="' .plugins_url('images/dashboard.png', __FILE__). '" />';
+}
+add_action('admin_head', 'sl_admin_favicon');
---
+++ readme.txt
@@ -0,0 +1,80 @@
+=== Dashboard Tweaks ===
+Contributors: senlin
+Donate link: http://senl.in/PPd0na
+Tags: dashboard, tweaks, admin bar, toolbar, menus, functions, styles
+Requires at least: 3.3-beta4
+Tested up to: 3.3
+Stable tag: 1.0
+
+A collection of Dashboard Tweaks that enables website developers to customize the WordPress Dashboard more towards their clients.
+
+== Description ==
+
+With the arrival of WordPress version 3.3 the WordPress Dashboard once again received an overhaul/makeover. The tweaks we have been using since 3.2 no longer cut it and that inspired me to develop this plugin for <strong>WordPress version 3.3 or higher</strong>.
+
+I have not included an options panel as this plugin is catered to developers and you are meant to play around with it. If you don't develop WordPress websites for clients, then most likely you won't have much use for this plugin and if you do, then be prepared to roll up your sleeves and get your hands dirty.
+
+A list of the tweaks included in the Dashboard Tweaks plugin:<br />
+- change the WordPress logo in the top-left of the admin bar with a custom admin logo<br />
+- link that logo to the home URL instead of the new wp-admin/about.php<br />
+- remove the entire sub-menu with links to Codex, forums and what not<br />
+- remove the Visit Site sub-menu under the site's name<br />
+- change "Howdy" into "Welcome back"<br />
+- add a special favicon for the Dashboard only (ideally different from the frontend, but up to you)<br />
+- change the tab title into a standard "Dashboard of [sitename]"<br />
+- remove icons in front of Dashboard menus<br />
+- reduce page heading size<br />
+- remove publish icon of Add new Post/Page screens<br />
+- add colored background to sidebar active sub-menu to add contrast<br />
+- add colored background to activated plugins to add contrast<br />
+- add custom footer text
+
+The plugin comes localized for use on sites other than the English language and/or on bi/multilingual websites.
+
+== Installation ==
+
+The Dashboard Tweaks plugin requires WordPress 3.3-beta4 or higher.
+
+1. Upload `dashboard-tweaks.zip` to the `/wp-content/plugins/` directory
+2a. Edit the files `dashboard-tweaks.php` and `/css/wp-admin.css` to your liking (link: `/wp-admin/plugin-editor.php?file=dashboard-tweaks/dashboard-tweaks.php`). See below what can be changed on which lines.
+2b. Alternatively you can unzip the file on your computer, do your edits and then upload the entire directory `dashboard-tweaks` to the `wp-content/plugins/` folder.
+3. Activate the plugin through the 'Plugins' menu in WordPress and enjoy your customized WordPress Dashboard!
+
+Change images for admin favicon and dashboard logo:<br />
+Use FTP to upload your own images, the admin favicon is called `dashboard.png` and the dashboard logo is called `adminbar-logo.png`; both files are located in the images folder of the dashboard-tweaks folder.
+
+Which lines of `dashboard-tweaks.php` can be edited?<br />
+lines 76-84: change Howdy to Welcome Back<br />
+lines 86-91: change tab title text<br/>
+lines 93-97: change custom footer text
+
+Which lines of `/css/wp-admin.css` can be edited?<br />
+lines 10-11: hover background color for admin-bar-wp-logo<br />
+line 17: change page heading size<br />
+line 24: change colored background to activated plugins to add contrast<br />
+lines 27-28: change colored background to sidebar active sub-menu to add contrast
+
+== Frequently Asked Questions ==
+
+= On the front-end the WordPress stuff in the admin bar is still visible! =
+
+Disable the admin bar for the user when viewing the site. Most clients want to see their own website, not that ugly admin bar.
+
+= Are you planning to add an options panel? =
+
+Maybe. I don't expect (frequent) updates will be necessary for this plugin, so basically your edits are protected.
+
+== Screenshots ==
+
+1. Dashboard after installation (This screen shot description corresponds to images/screenshot-1.jpg).
+2. Plugin screen after installation (This screen shot description corresponds to images/screenshot-2.jpg).
+
+== Changelog ==
+
+= 1.0 =
+* First version (stable)
+
+== Upgrade Notice ==
+
+= 1.0 =
+First release
